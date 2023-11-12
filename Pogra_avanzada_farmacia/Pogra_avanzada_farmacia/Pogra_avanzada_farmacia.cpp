3edd0c921bc2264495e10a0ee0f9a7d79bcb993f
Jose Garcia <57154305+Yipieight@users.noreply.github.com>
2023-11-12 09:03:05 -0600

    proyecto

--- Pogra_avanzada_farmacia/Pogra_avanzada_farmacia/Pogra_avanzada_farmacia.cpp
+++ Pogra_avanzada_farmacia/Pogra_avanzada_farmacia/Pogra_avanzada_farmacia.cpp
@@ -9,59 +9,59 @@
 
 using namespace std;
 
-class Medicamento {
-public:
-    string nom;
-    string numRegis;
-    string categ;
-    string prinAct;
-    string dosrec;
-
-    Medicamento() = default;
-
-    Medicamento(string nom, string numRegis, string categ, string prinAct, string dosrec)
-        : nom(nom), numRegis(numRegis), categ(categ), prinAct(prinAct), dosrec(dosrec) {}
-};
+class Inventario;
 
 class Proveedor {
 public:
-
     string nom;
     string nit;
-    string direcFiscal;
-    string direcContacto;
-    string celular;
-    string correo;
+    string direccionFiscal;
+    string direccionContacto;
+    string telefono;
+    string correoElectronico;
 
-    Proveedor(string nom, string nit, string direcFiscal, string direcContacto, string celular, string correo)
-        : nom(nom), nit(nit), direcFiscal(direcFiscal), direcContacto(direcContacto), celular(celular), correo(correo) {}
+    Proveedor(string nom, string nit, string direccionFiscal, string direccionContacto, string telefono, string correoElectronico)
+        : nom(nom), nit(nit), direccionFiscal(direccionFiscal), direccionContacto(direccionContacto), telefono(telefono), correoElectronico(correoElectronico) {}
 };
 
 class Inventario {
 public:
-    int cantStock;
+    int cantidadStock;
     string fechaCaducidad;
     Proveedor proveedor;
-    double precCompra;
-    double precVenta;
-   
-    Inventario(int cantStock, string fechaCaducidad, Proveedor proveedor, double precCompra, double precVenta)
-        : cantStock(cantStock), fechaCaducidad(fechaCaducidad), proveedor(proveedor), precCompra(precCompra), precVenta(precVenta) {}
+    double precioCompra;
+    double precioVenta;
+
+    Inventario(int cantidadStock, string fechaCaducidad, Proveedor proveedor, double precioCompra, double precioVenta)
+        : cantidadStock(cantidadStock), fechaCaducidad(fechaCaducidad), proveedor(proveedor), precioCompra(precioCompra), precioVenta(precioVenta) {};
+
+    Inventario() : cantidadStock(0), fechaCaducidad(""), proveedor(Proveedor("", "", "", "", "", "")), precioCompra(0.0), precioVenta(0.0) {}
+
+};
+
+
+class Medicamento {
+public:
+    string nom;
+    string numRegistro;
+    string categoria;
+    string principiosActivos;
+    string dosisRecomendada;
+    Inventario inventario;
+
+    Medicamento(string nom, string numRegistro, string categoria, string principiosActivos, string dosisRecomendada)
+        : nom(nom), numRegistro(numRegistro), categoria(categoria), principiosActivos(principiosActivos), dosisRecomendada(dosisRecomendada) {}
+
+    Medicamento() : nom(""), numRegistro(""), categoria(""), principiosActivos(""), dosisRecomendada(""), inventario(Inventario()) {}
 };
 
 class Farmacia {
 private:
-    list<Medicamento> medic;
-    list<Proveedor> prov;
-    list<Inventario> invent;
+    list<Medicamento> medicamentos;
+    list<Proveedor> proveedores;
+    list<Inventario> inventario;
 
 public:
-
-    void pausa() {
-        string uno;
-        cin >> uno;
-    }
-
     void gotoxy(int x, int y) {
         COORD coordinates;
         coordinates.X = x;
@@ -69,46 +69,10 @@ public:
         SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coordinates);
     }
 
-    Proveedor dar(string nombre) {
-        for (Proveedor med : prov)
-        {
-            if (nombre == med.nom) {
-                return med;
-            }
-        }
+    void pausa() {
+        string uno;
+        cin >> uno;
     }
-
-    void agregarAlInventario() {
-        string nombre;
-        cout << "Nombre del medicamento: " << endl;
-        cin >> nombre;
-
-        int cantidadStock;
-        string fechaCaducidad;
-        double precioCompra;
-        double precioVenta;
-
-        cout << "Ingrese la cantidad en Stock: " << endl;
-        cin >> cantidadStock;
-
-        cout << "Ingrese la fecha de caducidad: " << endl;
-        cin >> fechaCaducidad;
-
-        string nombreDeProveedor;
-        cout << "Ingrese el nombre del proveedor" << endl;
-        cin >> nombreDeProveedor;
-
-
-        cout << "Ingrese el precio de compra: " << endl;
-        cin >> precioCompra;
-
-        cout << "Ingrese el precio de venta: " << endl;
-        cin >> precioVenta;
-
-        Inventario inventario(cantidadStock, fechaCaducidad, dar(nombreDeProveedor), precioCompra, precioVenta);
-        agregarInventario(inventario);
-    }
-
     void titulo() {
 
         int xcol;
@@ -134,27 +98,75 @@ public:
             gotoxy(xcol, 3);
             cout << ' ';
             printf(titulo);
-            Sleep(30);
+            Sleep(10);
         }
 
         cout << endl;
     }
+    void agregarAlInventario() {
+        string nom;
+        cout << "Nombre del medicamento: " << endl;
+        cin >> nom;
+
+        auto it = find_if(medicamentos.begin(), medicamentos.end(), [&nom](const Medicamento& medicamentonom) {
+            return medicamentonom.nom == nom;
+            });
+
+        if (it != medicamentos.end()) {
+            int cantidadStock;
+            string fechaCaducidad;
+            double precioCompra;
+            double precioVenta;
+
+            cout << "Ingrese la cantidad en Stock: " << endl;
+            cin >> cantidadStock;
+
+            cout << "Ingrese la fecha de caducidad: " << endl;
+            cin >> fechaCaducidad;
+
+            string nomDeProveedor;
+            cout << "Ingrese el nom del proveedor" << endl;
+            cin >> nomDeProveedor;
+
+            cout << "Ingrese el precio de compra: " << endl;
+            cin >> precioCompra;
+
+            cout << "Ingrese el precio de venta: " << endl;
+            cin >> precioVenta;
+
+            auto proveedorit = find_if(proveedores.begin(), proveedores.end(), [&nomDeProveedor](const Proveedor& proveedornom) {
+                return proveedornom.nom == nomDeProveedor;
+                });
+
+            if (proveedorit != proveedores.end()) {
+                Inventario nuevoInventario(cantidadStock, fechaCaducidad, *proveedorit, precioCompra, precioVenta);
+                it->inventario = nuevoInventario;
+                inventario.push_back(nuevoInventario);
+                cout << "Medicamento agregado al inventario" << endl;
+            }
+            else {
+                cout << "Proveedor no encontrado" << endl;
+            }
+        }
+        else {
+            cout << "Medicamento no encontrado" << endl;
+        }
+    };
 
     void agregarMedicamento(const Medicamento& medicamento) {
-        medic.push_back(medicamento);
+        medicamentos.push_back(medicamento);
     }
 
     void agregarProveedor(const Proveedor& proveedor) {
-        prov.push_back(proveedor);
+        proveedores.push_back(proveedor);
     }
 
     void agregarInventario(const Inventario& item) {
-        invent.push_back(item);
-        cout << "hola";
+        inventario.push_back(item);
     }
 
-    Medicamento buscarMedicamentoPorNombre(const string& nom) {
-        for (const Medicamento& med : medic) {
+    Medicamento buscarMedicamentoPornom(const string& nom) {
+        for (const Medicamento& med : medicamentos) {
             if (med.nom == nom) {
                 return med;
             }
@@ -163,8 +175,8 @@ public:
     }
 
     Medicamento buscarMedicamentoPorPrincipioActivo(const string& principioActivo) {
-        for (const Medicamento& med : medic) {
-            if (med.prinAct == principioActivo) {
+        for (const Medicamento& med : medicamentos) {
+            if (med.principiosActivos == principioActivo) {
                 return med;
             }
         }
@@ -172,19 +184,19 @@ public:
     }
 
 
-    Medicamento medicamentoMasCaroProveedor(const string& nombreProveedor) {
+    Medicamento medicamentoMasCaroProveedor(const string& nomProveedor) {
         double precioMaximo = 0.0;
         Medicamento medicamentoMasCaro;
 
-        for (const Inventario& item : invent) {
-            if (item.proveedor.nom == nombreProveedor && item.precCompra > precioMaximo) {
-                precioMaximo = item.precCompra;
-                medicamentoMasCaro = buscarMedicamentoPorNombre(item.proveedor.nom);
+        for (const Medicamento& med : medicamentos) {
+            if (med.inventario.proveedor.nom == nomProveedor && med.inventario.precioCompra > precioMaximo) {
+                precioMaximo = med.inventario.precioCompra;
+                medicamentoMasCaro = med;
             }
         }
 
         if (precioMaximo == 0.0) {
-            throw runtime_error("Proveedor no encontrado o no se encontraron medic suministrados por el proveedor");
+            throw runtime_error("Proveedor no encontrado o no se hallaron medicamentos del proveedor");
         }
 
         return medicamentoMasCaro;
@@ -192,9 +204,9 @@ public:
 
     void generarInformeCSV() {
         ofstream archivo("informe_medicamentos.csv");
-        archivo << "nom,numRegis,categ,prinAct,dosrec" << endl;
-        for (const Medicamento& med : medic) {
-            archivo << med.nom << "," << med.numRegis << "," << med.categ << "," << med.prinAct << "," << med.dosrec << endl;
+        archivo << "nom;numRegistro;Categoria;PrincipiosActivos;DosisRecomendada" << endl;
+        for (const Medicamento& med : medicamentos) {
+            archivo << med.nom << ";" << med.numRegistro << ";" << med.categoria << ";" << med.principiosActivos << ";" << med.dosisRecomendada << endl;
         }
         archivo.close();
     }
@@ -202,61 +214,54 @@ public:
     double calcularPrecioPromedio() {
         double total = 0.0;
         int count = 0;
-        for (const Inventario& item : invent) {
-            total += item.precVenta;
+        for (const Inventario& item : inventario) {
+            total += item.precioVenta;
             count++;
         }
         return total / count;
     }
 
     void agregarMedicamentoDesdeMenu() {
-        system("cls");
         string nom;
-        string numRegis;
-        string categ;
-        string prinAct;
-        string dosrec;
+        string numRegistro;
+        string categoria;
+        string principiosActivos;
+        string dosisRecomendada;
 
         cout << "=== Agregar Medicamento ===" << endl;
-        cout << "Ingrese el nom del medicamento: ";
+        cout << "Ingrese el nombre del medicamento: ";
         cin.ignore();
         getline(cin, nom);
 
         cout << "Ingrese el número de registro: ";
-        cin.ignore();
-        cin >> numRegis;
+        cin >> numRegistro;
 
         cout << "Ingrese la categoría: ";
         cin.ignore();
-        getline(cin, categ);
+        getline(cin, categoria);
 
         cout << "Ingrese los principios activos: ";
         cin.ignore();
-        getline(cin, prinAct);
+        getline(cin, principiosActivos);
 
         cout << "Ingrese la dosis recomendada: ";
         cin.ignore();
-        getline(cin, dosrec);
+        getline(cin, dosisRecomendada);
 
-        Medicamento nuevoMedicamento(nom, numRegis, categ, prinAct, dosrec);
+        Medicamento nuevoMedicamento(nom, numRegistro, categoria, principiosActivos, dosisRecomendada);
         agregarMedicamento(nuevoMedicamento);
         cout << "Medicamento agregado con éxito." << endl;
-
-        pausa();
     }
     void agregarProveedorDesdeMenu() {
-
-        system("cls");
-
         string nom;
         string nit;
-        string direcFiscal;
-        string direcContacto;
-        string celular;
-        string correo;
+        string direccionFiscal;
+        string direccionContacto;
+        string telefono;
+        string correoElectronico;
 
         cout << "=== Agregar Proveedor ===" << endl;
-        cout << "Ingrese el nom del proveedor: ";
+        cout << "Ingrese el nombre del proveedor: ";
         cin.ignore();
         getline(cin, nom);
 
@@ -265,41 +270,45 @@ public:
 
         cout << "Ingrese la dirección fiscal: ";
         cin.ignore();
-        getline(cin, direcFiscal);
+        getline(cin, direccionFiscal);
 
         cout << "Ingrese la dirección de contacto: ";
         cin.ignore();
-        getline(cin, direcContacto);
+        getline(cin, direccionContacto);
 
         cout << "Ingrese el número de teléfono: ";
-        cin >> celular;
+        cin >> telefono;
 
         cout << "Ingrese el correo electrónico: ";
-        cin >> correo;
+        cin >> correoElectronico;
 
-        Proveedor nuevoProveedor(nom, nit, direcFiscal, direcContacto, celular, correo);
+        Proveedor nuevoProveedor(nom, nit, direccionFiscal, direccionContacto, telefono, correoElectronico);
         agregarProveedor(nuevoProveedor);
         cout << "Proveedor agregado con éxito." << endl;
-
-        pausa();
     }
 
-
-    void mostrarMenu() {
+    void printMenu() {
         int opcion;
+
+        string nom;
+        string principioActivo;
+        string nomProveedor;
+
+
+
         do {
             system("cls");
             titulo();
             cout << endl;
-            cout << "1. Consultar medicamento por nombre" << endl;
-            cout << "2. Consultar medicamento por principio activo" << endl;
-            cout << "3. Agregar medicamento" << endl;
-            cout << "4. Agregar proveedor" << endl;
-            cout << "5. Generar informe CSV de medicicamentos" << endl;
-            cout << "6. Calcular precio promedio de medicamentos" << endl;
-            cout << "7. Encontrar medicamento más caro de un proveedor" << endl;
-            cout << "8. Agregar inventario" << endl;
-            cout << "9. Salir" << endl;
+            cout << "1-Detalles de medicamento por nombre" << endl;
+            cout << "2-Detalles de medicamento por principio activo" << endl;
+            cout << "3-Generar informe CSV de medicamentos" << endl;
+            cout << "4-Calcular precio promedio de medicamentos" << endl;
+            cout << "5-Encontrar medicamento más caro de un proveedor" << endl;
+            cout << "6-Insertar medicamento" << endl;
+            cout << "7-Insertar proveedor" << endl;
+            cout << "8-Insertar a inventario" << endl;
+            cout << "9-Salir" << endl;
             cout << "Ingrese su opción: ";
             cin >> opcion;
 
@@ -307,18 +316,17 @@ public:
             case 1:
             {
                 system("cls");
-                string nom;
-                cout << "Ingrese el nom del medicamento: ";
+                cout << "Ingrese el nombre del medicamento: ";
                 cin.ignore();
                 getline(cin, nom);
                 try {
-                    Medicamento med = buscarMedicamentoPorNombre(nom);
+                    Medicamento med = buscarMedicamentoPornom(nom);
                     cout << "Información del medicamento:" << endl;
                     cout << "nom: " << med.nom << endl;
-                    cout << "Número de Registro: " << med.numRegis << endl;
-                    cout << "Categoría: " << med.categ << endl;
-                    cout << "Principios Activos: " << med.prinAct << endl;
-                    cout << "Dosis Recomendada: " << med.dosrec << endl;
+                    cout << "Número de Registro: " << med.numRegistro << endl;
+                    cout << "Categoría: " << med.categoria << endl;
+                    cout << "Principios Activos: " << med.principiosActivos << endl;
+                    cout << "Dosis Recomendada: " << med.dosisRecomendada << endl;
                 }
                 catch (const runtime_error& e) {
                     cerr << e.what() << endl;
@@ -329,7 +337,6 @@ public:
             case 2:
             {
                 system("cls");
-                string principioActivo;
                 cout << "Ingrese el principio activo del medicamento: ";
                 cin.ignore();
                 getline(cin, principioActivo);
@@ -337,62 +344,76 @@ public:
                     Medicamento med = buscarMedicamentoPorPrincipioActivo(principioActivo);
                     cout << "Información del medicamento:" << endl;
                     cout << "nom: " << med.nom << endl;
-                    cout << "Número de Registro: " << med.numRegis << endl;
-                    cout << "Categoría: " << med.categ << endl;
-                    cout << "Principios Activos: " << med.prinAct << endl;
-                    cout << "Dosis Recomendada: " << med.dosrec << endl;
+                    cout << "Número de Registro: " << med.numRegistro << endl;
+                    cout << "Categoría: " << med.categoria << endl;
+                    cout << "Principios Activos: " << med.principiosActivos << endl;
+                    cout << "Dosis Recomendada: " << med.dosisRecomendada << endl;
                 }
                 catch (const runtime_error& e) {
                     cerr << e.what() << endl;
                 }
                 pausa();
             }
+
             break;
             case 3:
-                agregarMedicamentoDesdeMenu();
+                system("cls");
+                generarInformeCSV();
+                pausa();
                 break;
             case 4:
-                agregarProveedorDesdeMenu();
+                system("cls");
+                cout << "Precio promedio de los medicamentos: Q" << calcularPrecioPromedio() << endl;
+                pausa();
                 break;
             case 5:
+                system("cls");
+                cout << "Ingrese el nom del proveedor: ";
+                cin.ignore();
+                getline(cin, nomProveedor);
+                try {
+                    Medicamento masCaro = medicamentoMasCaroProveedor(nomProveedor);
+                    cout << "Medicamento más caro suministrado por " << nomProveedor << ": " << masCaro.nom << endl;
+                }
+                catch (const runtime_error& e) {
+                    cerr << e.what() << endl;
+                }
                 generarInformeCSV();
+                pausa();
+
                 break;
             case 6:
                 system("cls");
-                cout << "Precio promedio de los medic: Q" << calcularPrecioPromedio() << endl;
+                agregarMedicamentoDesdeMenu();
                 pausa();
                 break;
             case 7:
             {
                 system("cls");
-                string nombreProveedor;
-                cout << "Ingrese el nombre del proveedor: ";
-                cin.ignore();
-                getline(cin, nombreProveedor);
-                try {
-                    Medicamento masCaro = medicamentoMasCaroProveedor(nombreProveedor);
-                    cout << "Medicamento más caro suministrado por " << nombreProveedor << ": " << masCaro.nom << endl;
-                }
-                catch (const runtime_error& e) {
-                    cerr << e.what() << endl;
-                }
+                agregarProveedorDesdeMenu();
                 pausa();
+
             }
             break;
             case 8:
+                system("cls");
                 agregarAlInventario();
+                pausa();
+
+                break;
             case 9:
+                system("cls");
                 cout << "Saliendo del programa." << endl;
+                Sleep(40);
                 break;
             default:
-                cout << "Opcion no válida. Intente de nuevo." << endl;
+                cout << "Opción no válida. Intente de nuevo." << endl;
             }
         } while (opcion != 9);
     }
-
 };
 
 int main() {
     Farmacia farma;
-    farma.mostrarMenu();
+    farma.printMenu();
 }
